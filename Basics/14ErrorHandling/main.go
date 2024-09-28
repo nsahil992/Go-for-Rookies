8618fc1c59615dcf57577139c294d2e0e8b7917f
Sahil Naik <nsahil992@gmail.com>
2024-09-28 16:03:56 +0530

    feat: basics

--- Basics/14ErrorHandling/main.go
+++ Basics/14ErrorHandling/main.go
@@ -3,15 +3,22 @@ import (
 	"fmt"
 )
 
-func divide(a, b float64) float64 {
+func divide(a, b float64) (float64, string) {
 	if b == 0 {
-		return 0
+		return 0, "Denominator must not be 0"
 	}
-	return a / b
+	return a / b, "nil"
+}
+
+func getUserData() (string, int) {
+	return "Sahil", 21
 }
 
 func main() {
 	fmt.Println("Error handling in functions:")
-	ans := divide(10, 4)
+	ans,_ := divide(10, 0)
 	fmt.Println("The division is: ",ans)
+
+	name,_ := getUserData()
+	fmt.Println("Welcome,",name)
 }
