ee3f0c3537d63aafbb8a1cc19652d2c210f6df70
Sahil Naik <nsahil992@gmail.com>
2024-10-19 21:48:31 +0530

    feat: basics

---
+++ Basics/23Defer/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "fmt"
+    "os"
+)
+
+func main() {
+    file, err := os.Open("example.txt")
+    if err != nil {
+        fmt.Println("Error opening file:", err)
+        return
+    }
+    defer file.Close()
+    fmt.Println("File is opened and ready for operations.")
+}
