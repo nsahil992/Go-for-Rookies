1b98b530b4764d5cfa276a9d1d2848bd5ca87320
Sahil Naik <nsahil992@gmail.com>
2024-11-20 20:33:45 +0530

    feat: basics

--- Basics/23Defer/main.go
+++ Basics/23Defer/main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 )
 
 func main() {
-    file, err := os.Open("example.txt")
-    if err != nil {
-        fmt.Println("Error opening file:", err)
-        return
-    }
-    defer file.Close()
-    fmt.Println("File is opened and ready for operations.")
+
+	fmt.Println("Starting main function")
+	defer fmt.Println("This is a deferred function")
+	fmt.Println("This will print first")
+
+	file, err := os.Open("example.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+	fmt.Println("File is opened and ready for operations.")
 }
