3158692de0130532bbdcd5199477f7d8356ed70e
Sahil Naik <nsahil992@gmail.com>
2025-01-06 19:08:38 +0530

    feat: basics

--- Basics/20Interfaces/main.go
+++ Basics/20Interfaces/main.go
@@ -1,46 +1,33 @@
 package main
 
-import (
-    "fmt"
-    "math"
-)
-
-type geometry interface {
-    area() float64
-    perim() float64
-}
-
-type rect struct {
-    width, height float64
-}
-type circle struct {
-    radius float64
-}
-
-func (r rect) area() float64 {
-    return r.width * r.height
-}
-func (r rect) perim() float64 {
-    return 2*r.width + 2*r.height
-}
-
-func (c circle) area() float64 {
-    return math.Pi * c.radius * c.radius
-}
-func (c circle) perim() float64 {
-    return 2 * math.Pi * c.radius
-}
-
-func measure(g geometry) {
-    fmt.Println(g)
-    fmt.Println(g.area())
-    fmt.Println(g.perim())
-}
+import "fmt"
 
 func main() {
-    r := rect{width: 3, height: 4}
-    c := circle{radius: 5}
+	dog := Dog{}
+	cat := Cat{}
 
-    measure(r)
-    measure(c)
+	introduce(dog)
+	introduce(cat)
+}
+
+type Speaker interface {
+	Speak() string
+}
+
+type Dog struct {
+}
+
+type Cat struct {
+}
+
+func (d Dog) Speak() string {
+	return "Meow!"
+}
+
+func (c Cat) Speak() string {
+	return "Meow!"
+}
+
+func introduce(speaker Speaker) {
+	fmt.Println(speaker.Speak())
 }
