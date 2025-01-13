06542d2627998614f627246529ce8f2ac1718ff5
Sahil Naik <nsahil992@gmail.com>
2025-01-13 18:32:16 +0530

    feat: advanced

---
+++ Advanced/02Modules/02AccessingPackage/greetings/greetings.go
@@ -0,0 +1,5 @@
+package greetings
+
+const HelloMessage = "Hello DevOps Engineer!"
+
+var Greeting = "Welcome to learning Go from Greetings package"
---
+++ Advanced/02Modules/02AccessingPackage/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"golearn/Advanced/02Modules/02AccessingPackage/greetings"
+)
+
+func main() {
+	fmt.Println(greetings.HelloMessage)
+	fmt.Println(greetings.Greeting)
+}
