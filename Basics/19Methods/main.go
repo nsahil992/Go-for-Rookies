40054f69d9adc9171469c0cf3f4b830faf68c3a5
Sahil Naik <nsahil992@gmail.com>
2025-01-15 06:41:26 +0530

    feat: basics

--- Basics/19Methods/main.go
+++ Basics/19Methods/main.go
@@ -1,19 +1,27 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+type Car struct {
+	name  string
+	model int
+	color string
+}
+
+func (car1 Car) enginestart() {
+	fmt.Printf("The %v %v colour %v has just started.\n", car1.model, car1.color, car1.name)
+}
+
+func (car2 Car) highestSpeed() {
+	fmt.Printf("The %v %v is the fastest car in the world\n", car2.model, car2.name)
+}
 
 func main() {
-	person := Person{Name: "Sahil", Age: 21}
-	person.greet()
-}
+	var fastestCar Car
+	fastestCar.name = "Bugatti Chiron Supersport"
+	fastestCar.model = 2016
+	fastestCar.color = "Exposed Blue Carbon Fiber With Silve"
 
-type Person struct {
-	Name string
-	Age  int
-}
-
-func (p Person) greet() {
-	fmt.Println("Hello, My name is ", p.Name, "and I am ", p.Age, "years old.")
+	fastestCar.highestSpeed()
+	fastestCar.enginestart()
 }
