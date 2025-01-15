207b8479386b690d525eb79523a54048a47d6f2d
Sahil Naik <nsahil992@gmail.com>
2025-01-15 10:43:54 +0530

    feat: basics

--- Basics/20Interfaces/main.go
+++ Basics/20Interfaces/main.go
@@ -2,32 +2,41 @@ package main
 
 import "fmt"
 
+// Defining an interface `Vehicle`
+type Vehicle interface {
+	enginestart()  // Method to start the engine
+	highestSpeed() // Method to declare the highest speed
+}
+
+// Defining the Car struct
+type Car struct {
+	name  string
+	model int
+	color string
+}
+
+// `Car` implements the `enginestart` method
+func (car Car) enginestart() {
+	fmt.Printf("The %v %v colour %v has just started.\n", car.model, car.color, car.name)
+}
+
+// `Car` implements the `highestSpeed` method
+func (car Car) highestSpeed() {
+	fmt.Printf("The %v %v is the fastest car in the world\n", car.model, car.name)
+}
+
+// Main function
 func main() {
-	dog := Dog{}
-	cat := Cat{}
+	// Creating a Car instance
+	var fastestCar Car
+	fastestCar.name = "Bugatti Chiron Supersport"
+	fastestCar.model = 2016
+	fastestCar.color = "Exposed Blue Carbon Fiber With Silver"
 
-	introduce(dog)
-	introduce(cat)
-}
+	// Declare a variable of type `Vehicle` and assign `fastestCar`
+	var myVehicle Vehicle = fastestCar
 
-type Speaker interface {
-	Speak() string
-}
-
-type Dog struct {
-}
-
-type Cat struct {
-}
-
-func (d Dog) Speak() string {
-	return "Meow!"
-}
-
-func (c Cat) Speak() string {
-	return "Meow!"
-}
-
-func introduce(speaker Speaker) {
-	fmt.Println(speaker.Speak())
+	// Call methods using the interface
+	myVehicle.highestSpeed()
+	myVehicle.enginestart()
 }
